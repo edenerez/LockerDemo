472e8e32d13493e7f445d28509784e24060f8fcd
Eden Shalom Erez Avrahami <edenerez@gmail.com>
2020-12-29 10:26:51 +0200

    Add files via upload

---
+++ WpfApp1/Elocker.cs
@@ -0,0 +1,183 @@
+﻿using DcdzDriverBoardLib;
+using DcdzMsCommLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Elocker
+    {
+        private readonly DriverBoardClass _driverboard = new DriverBoardClass();
+
+
+        //打开柜子串口
+        public bool open(string comName)
+        {
+            try
+            {
+                uint funcret = _driverboard.Open(comName);
+                if (funcret == 0)
+                    return true;
+                return false;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+           
+        }
+
+        //打开箱门 （driverID=0,boxID=1:打开1号柜子的1号门）
+        public bool openBox(int driverID, int boxID)
+        {
+            try
+            {
+                byte ret;
+                int funcret = _driverboard.OpenBox((byte)driverID, (byte)boxID, out ret);
+                if (funcret == 0)
+                    return true;
+                return false;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+          
+
+        }
+
+        //获取1号副柜的单个门状态  
+        public void QueryBoxStatus(int boxId,out int OpenStatus, out int Article)
+        {
+            try
+            {
+                byte[,] ret = (byte[,])NeedState(0, true, true, false, false);
+                OpenStatus = ret[1, boxId-1]==0?1:0;
+                Article = ret[0, boxId-1];
+            }
+            catch (Exception)
+            {
+                OpenStatus = 0;
+                Article = 0;
+                throw;
+            }
+          
+        }
+
+        //获取1号副柜的所有门状态
+        public byte[,] queryAllBoxStatus(int driverID)
+        {
+            return (byte[,])NeedState(driverID, true, true, false, false);
+
+        }
+        private Array NeedState(int drivernum, bool openflag, bool articleflag, bool warnflag, bool powerflag)
+        {
+            try
+            {
+                int ret;
+                Array doorstate;
+
+                ret = _driverboard.GetNeedState((byte)drivernum, articleflag, openflag, warnflag, powerflag, out doorstate);
+
+                if (ret == 0)
+                    return doorstate;
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+    }
+
+    public class Scanner
+    {
+        protected readonly DcdzMsComm DcdzMsComm = new DcdzMsCommClass();
+        private OnReceived _onReceived=null ;
+        public void startScanenr(OnReceived onReceived)
+        {
+            _onReceived += onReceived;
+        }
+        public bool open(string comName)
+        {
+            try
+            {
+                if (DcdzMsComm.Open(comName) == 0)
+                {
+                    DcdzMsComm.SetCommState(9600, 8, 0, 0);
+                    DcdzMsComm.SetCommTimeouts(500, 50, 3000, 500, 500);
+                    DcdzMsComm.SetReadWriteQueue(100, 100);
+                    IMsCommEvent imsComEvent = DcdzMsComm.GetCommEvent();
+                    imsComEvent.COMM_EV_RXCHAR = true;
+                    imsComEvent.COMM_EV_ERR = true;
+                    DcdzMsComm.OpenCommEventNotification(imsComEvent);
+                    DcdzMsComm.CommEvent += DcdzMsComm_CommEvent;
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+           
+         
+           
+        }
+
+        private string _datastr = "";
+        void DcdzMsComm_CommEvent(uint uEventMark)
+        {
+            try
+            {
+                if (uEventMark == 0)
+                {
+                    int datalen = (int)DcdzMsComm.GetInQueue();
+                    Array databuffer = new byte[datalen];
+                    DcdzMsComm.Read(ref databuffer, datalen);
+
+                    #region 结果处理
+
+
+                    #endregion
+
+                    foreach (byte item in (byte[])databuffer)
+                    {
+                        if (item == 0x0D && !string.IsNullOrEmpty(_datastr))
+                        {
+                            //扫描枪扫描出现乱码的问题
+                            if ("html".Equals(_datastr.ToLower().Trim()))
+                            {
+                                _datastr = "";
+                                break;
+                            }
+
+                            _onReceived(_datastr);
+                              _datastr = "";
+                            break;
+                        }
+
+                        _datastr += Convert.ToChar(item);
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+    public delegate void OnReceived(string dataInfo);
+
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Scanner scanner ;
+        Elocker elocker;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            scanner = new Scanner();
+            
+            elocker = new Elocker();
+
+            int OpenStatus1 = 0;
+            int Article1 = 0;
+            if (elocker.open("COM1"))
+            {
+                elocker.QueryBoxStatus(11, out OpenStatus1, out Article1);
+                if (OpenStatus1 == 1)
+                {
+                    Box1.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                }
+                elocker.QueryBoxStatus(12, out OpenStatus1, out Article1);
+                if (OpenStatus1 == 1)
+                {
+                    Box2.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                }
+                elocker.QueryBoxStatus(13, out OpenStatus1, out Article1);
+                if (OpenStatus1 == 1)
+                {
+                    Box3.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                }
+                elocker.QueryBoxStatus(14, out OpenStatus1, out Article1);
+                if (OpenStatus1 == 1)
+                {
+                    Box4.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                }
+                elocker.QueryBoxStatus(15, out OpenStatus1, out Article1);
+                if (OpenStatus1 == 1)
+                {
+                    Box5.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                }
+            }
+        }
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (scanner != null)
+            {
+                scanner.startScanenr(OnReceived);
+            }
+            else
+                MessageBox.Show("fail 0");
+
+        }
+        private void OnReceived(string dataInfo)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                scannerInfo.Text = dataInfo;
+            }));
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (scanner != null)
+            {
+                if (scanner.open(comScanner.Text))
+                {
+                    MessageBox.Show("succeed");
+                }
+                else
+                    MessageBox.Show("fail 2");
+
+            }
+            else
+                MessageBox.Show("fail ");
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            if (elocker != null)
+             {
+                if (elocker.openBox(0, Int32.Parse(txtBox.Text)))
+                {
+                    MessageBox.Show("succeed to open a BOX");
+                }
+                else
+                    MessageBox.Show("fail to open a BOX");
+            }
+            else
+                MessageBox.Show("fail to connect to COM port");
+            
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (elocker != null)
+            {
+               
+                if (elocker.openBox(0, Int32.Parse(Box1.Content.ToString())))
+                {
+                    MessageBox.Show("succeed");
+                }
+                else
+                    MessageBox.Show("fail ");
+            }
+        }
+
+      
+
+        private void Box2_Click(object sender, RoutedEventArgs e)
+        {
+            Button current_box = ((Button)sender);
+            current_box.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            //Box2
+            // Box2.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 255));
+            if (elocker != null)
+            {
+                if (elocker.openBox(0, Int32.Parse(current_box.Content.ToString())))
+                {
+                    MessageBox.Show("succeed");
+                }
+                else
+                    MessageBox.Show("fail ");
+            }
+        }
+
+        private void comLocker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
